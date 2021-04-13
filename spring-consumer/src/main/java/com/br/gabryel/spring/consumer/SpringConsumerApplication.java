2b01aeb52dd820845a3df3e894c0ca53c15f16b7
gabryelBoeira <gabryel@asaplog.com.br>
2021-04-13 14:18:34 -0300

    Configuração inicial para o projeto consumer sobre o parking-lot

--- spring-consumer/src/main/java/com/br/gabryel/spring/consumer/SpringConsumerApplication.java
+++ spring-consumer/src/main/java/com/br/gabryel/spring/consumer/SpringConsumerApplication.java
@@ -1,8 +1,12 @@
 package com.br.gabryel.spring.consumer;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@EnableRabbit
+@ComponentScan("com.br.gabryel.spring")
 @SpringBootApplication
 public class SpringConsumerApplication {
 
